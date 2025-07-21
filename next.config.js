9154b6a0e6d494d777f963c90f7f4152cfde33f7
dayrade <iang@zimtra.com>
2025-07-21 18:54:06 +0100

    Add project configuration files

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'localhost',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'media.licdn.com',
+      'pbs.twimg.com'
+    ],
+  },
+}
+
+module.exports = nextConfig
